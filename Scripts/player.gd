e3298c834743b6f5062575be80470d429a4800de
degen21 <nftdegen21@gmail.com>
2024-07-18 21:48:37 -0400

    update movement and collision detection

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,23 +1,29 @@
 extends CharacterBody2D
 
-@export var currentPostion = [-8,120]
+@export var currentPosition = Vector2(-8, 120)
 
 @onready var sprite_2d = $Sprite2D
 
 func _input(event):
-	print(self.position)	
-	if event.is_action_pressed("left"):
-		currentPostion[0] -= 16
-		sprite_2d.flip_h= true
-	elif event.is_action_pressed("right"):
-		currentPostion[0] += 16
-		sprite_2d.flip_h= false
-	elif event.is_action_pressed("up"):
-		currentPostion[1] -= 16
-	elif event.is_action_pressed("down"):
-		currentPostion[1] += 16
-		
-	self.position = Vector2(currentPostion[0], currentPostion[1])	
+	var move_offset = Vector2.ZERO
 
-	move_and_slide()
-	
+	if event.is_action_pressed("left"):
+		move_offset.x -= 16
+		sprite_2d.flip_h = true
+	elif event.is_action_pressed("right"):
+		move_offset.x += 16
+		sprite_2d.flip_h = false
+	elif event.is_action_pressed("up"):
+		move_offset.y -= 16
+	elif event.is_action_pressed("down"):
+		move_offset.y += 16
+
+	print(self.position)
+	# Attempt to move the player, and handle collision detection
+	var collision_info = move_and_collide(move_offset)
+	if collision_info:
+		print("Blocked by wall", collision_info.get_position())
+	else:
+		# Only update position if no collision occurred
+		currentPosition += move_offset
+		self.position = currentPosition
