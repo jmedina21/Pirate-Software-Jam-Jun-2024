28bdc51b03193c3fef99e5acfec2f61e828a83f7
jpmm21 <jmedinamulet@gmail.com>
2024-08-04 11:53:56 -0400

    using load file instead of packed scene

--- Scripts/hatch.gd
+++ Scripts/hatch.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-@export var target_scene: PackedScene
+@export var target_scene: String
 @onready var animation_player = $AnimationPlayer
 @onready var color_rect = $CanvasLayer/ColorRect
 
@@ -9,4 +9,4 @@ func _on_body_entered(body):
 		animation_player.play("fade_out")
 		
 func _on_animation_player_animation_finished(anim_name):
-		get_tree().change_scene_to_packed(target_scene)
+		get_tree().change_scene_to_file(target_scene)
