4dd94fc235798e80d4e0b730210d8448742d1c36
jpmm21 <jmedinamulet@gmail.com>
2024-08-04 13:33:32 -0400

    add necromancer death animation

--- Scripts/necromancer.gd
+++ Scripts/necromancer.gd
@@ -1,6 +1,10 @@
 extends Area2D
 signal boss_died(position)
+@onready var death_animation = $death_animation
 
 func on_death():
+	death_animation.play("death")
+
+func _on_death_animation_animation_finished(anim_name):
 	emit_signal("boss_died", global_position)
 	queue_free()
